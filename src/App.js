76b3f9ed5ad3dcf4b9ec13eea14071fa71ee2e56
sergioness <serhii.krasnovidov@gmail.com>
2023-10-01 00:50:44 +0100

    measure distance

--- src/App.js
+++ src/App.js
@@ -1,43 +1,137 @@
-import logo from './logo.svg';
-import './App.css';
-import { Marker, Popup, MapContainer, TileLayer, useMap } from 'react-leaflet';
+import "./App.css";
+import "leaflet/dist/leaflet.css";
+import {
+  Popup,
+  MapContainer,
+  TileLayer,
+  useMap,
+  Polyline,
+} from "react-leaflet";
+import { useMemo, useRef, useState } from "react";
 
 async function readDocument(rawFile, type = "application/xml") {
   const reader = new FileReader();
   const file = new Promise((resolve, reject) => {
-    reader.addEventListener('loadend', (event) => {
+    const onLoadEnd = (event) => {
       resolve(reader.result);
-    })
+      reader.removeEventListener("loadend", onLoadEnd);
+    };
+    const onError = (event) => {
+      reject(reader.result);
+      reader.removeEventListener("error", onLoadEnd);
+    };
+    reader.addEventListener("loadend", onLoadEnd);
+    reader.addEventListener("error", onError);
   });
   reader.readAsText(rawFile);
-  const doc = new DOMParser().parseFromString(await file, type)
+  const doc = new DOMParser().parseFromString(await file, type);
   return doc;
 }
 
-async function onFileUpload(event) {
-  if (!event.target?.files.length ) return;
-  const [ file ] = event.target.files;
-  const document = await readDocument(file);
-  console.debug('document:%o', document);
-}
-function App() {
+function DistanceMeasurer({ positions }) {
+  const map = useMap();
+  if (!positions.length) {
+    return <></>;
+  }
+  const pointA = positions[0];
+  const pointB = positions[positions.length - 1];
+  const [distance] = positions.reduce(
+    ([sum, prevPoint], position) => [
+      sum + map.distance(prevPoint, position),
+      position,
+    ],
+    [0, pointA]
+  );
   return (
-    <main style={{ width: '100%', height: '100%', gap: '10px', display: 'flex', flexDirection  : 'column', justifyContent: 'center', alignItems: 'center'}}>
-      <label>
-        Your file please
-        <input onChange={onFileUpload} type='file' />
-      </label>
-      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
-  <TileLayer
-    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-  />
-  <Marker position={[51.505, -0.09]}>
-    <Popup>
-      A pretty CSS3 popup. <br /> Easily customizable.
-    </Popup>
-  </Marker>
-</MapContainer>
+    <>
+      <Popup>Distance: {Number(distance).toFixed(2)} meters</Popup>
+    </>
+  );
+}
+
+const mapGeoObjToTuple = ({ lat, lon }) => [lat, lon];
+
+function App() {
+  const { current: pathOptions } = useRef({ color: "red", weight: 2 });
+  const { current: pathOptionsSelected } = useRef({
+    color: "purple",
+    weight: 3,
+  });
+  const { current: pathOptionsSelectedHover } = useRef({ weight: 5 });
+  const { current: DATE } = useRef({
+    START: new Date("2023-09-24T15:09:12Z"),
+    END: new Date("2023-09-24T15:30:12Z"),
+  });
+  const [doc, setDoc] = useState(null);
+  async function onFileUpload(event) {
+    if (!event.target?.files.length) return;
+    const [file] = event.target.files;
+    const doc = await readDocument(file);
+    setDoc(doc);
+  }
+  const points = useMemo(() => {
+    if (!doc) return [];
+    const xpath = "//*[local-name()='trkpt']";
+    /** @type {XPathResult} */
+    const nodes = doc.evaluate(xpath, doc, null, XPathResult.ANY_TYPE, null);
+    const pts = [];
+    for (let node = nodes.iterateNext(); node; node = nodes.iterateNext()) {
+      const lat = node.getAttribute("lat");
+      const lon = node.getAttribute("lon");
+      const time = node.querySelector("time")?.textContent;
+      pts.push({ lat, lon, time });
+    }
+    return pts;
+  }, [doc]);
+  const positions = useMemo(() => points.map(mapGeoObjToTuple), [points]);
+  const positionsSelected = useMemo(
+    () =>
+      points
+        .filter(
+          ({ time }) =>
+            time && DATE.START <= new Date(time) && new Date(time) <= DATE.END
+        )
+        .map(mapGeoObjToTuple),
+    [points]
+  );
+  return (
+    <main>
+      <div>
+        <label>
+          Your file please
+          <input onChange={onFileUpload} type="file" accept=".gpx" />
+        </label>
+      </div>
+      <div style={{ width: "1000px", height: "800px" }}>
+        <MapContainer
+          style={{ width: "100%", height: "100%" }}
+          center={positions[positions.length - 1] ?? [51.505, -0.09]}
+          zoom={13}
+          scrollWheelZoom={false}
+        >
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Polyline pathOptions={pathOptions} positions={positions} />
+          <Polyline
+            pathOptions={pathOptionsSelected}
+            positions={positionsSelected}
+            eventHandlers={{
+              mouseover: (e) => {
+                e.target.setStyle(pathOptionsSelectedHover);
+                e.target.openPopup();
+              },
+              mouseout: (e) => {
+                e.target.setStyle(pathOptionsSelected);
+                e.target.closePopup();
+              },
+            }}
+          >
+            <DistanceMeasurer positions={positionsSelected} />
+          </Polyline>
+        </MapContainer>
+      </div>
     </main>
   );
 }
