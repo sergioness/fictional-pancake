ee18af9195042fda55b64b982a4446c18250ff2b
Serhii Krasnovidov (admin) <sk@192.168.1.229>
2023-09-30 21:46:05 +0100

    plug leaflet

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import { Marker, Popup, MapContainer, TileLayer, useMap } from 'react-leaflet';
 
+async function readDocument(rawFile, type = "application/xml") {
+  const reader = new FileReader();
+  const file = new Promise((resolve, reject) => {
+    reader.addEventListener('loadend', (event) => {
+      resolve(reader.result);
+    })
+  });
+  reader.readAsText(rawFile);
+  const doc = new DOMParser().parseFromString(await file, type)
+  return doc;
+}
+
+async function onFileUpload(event) {
+  if (!event.target?.files.length ) return;
+  const [ file ] = event.target.files;
+  const document = await readDocument(file);
+  console.debug('document:%o', document);
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main style={{ width: '100%', height: '100%', gap: '10px', display: 'flex', flexDirection  : 'column', justifyContent: 'center', alignItems: 'center'}}>
+      <label>
+        Your file please
+        <input onChange={onFileUpload} type='file' />
+      </label>
+      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+  <TileLayer
+    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+  />
+  <Marker position={[51.505, -0.09]}>
+    <Popup>
+      A pretty CSS3 popup. <br /> Easily customizable.
+    </Popup>
+  </Marker>
+</MapContainer>
+    </main>
   );
 }
 
